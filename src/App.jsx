0f9efda998ed53d621b43930cf8d0823eb0c4ba1
echicasprado <everchicasp@gmail.com>
2025-11-10 17:47:14 -0600

    Configs

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-900">
+      <h1 className="text-4xl font-bold mb-4">🚀 React + Vite + TailwindCSS</h1>
+      <p className="text-lg text-gray-600">Configurado con Yarn y listo para trabajar.</p>
+    </div>
+  );
+}
+
+export default App;
